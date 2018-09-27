2a1377a6f72e3c0fb225f4b723c3b95c9a12fa6c
Giovane Roggen <giovane@forttiori.com.br>
2018-09-27 20:32:42 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4000/register',
+    apiUrl: 'https://api.weenvoyer.com/bff/v1/contacts/merge',
     customerId: 2,
     token: 'F69C9E7D5E00B1C38010751BABC2D28A575613F4BED3851F50FC5F4055134C7D'
 };
