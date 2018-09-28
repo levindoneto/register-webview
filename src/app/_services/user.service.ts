1cae53024761a50ec86c1a5849711c38f4c7acd9
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-09-28 20:35:49 +0000

    Add getting facebook id

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { User } from '../_models';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
@@ -10,7 +9,6 @@ export class UserService {
 
     // Register user in the otoneuro's contacts
     register(user: User) {
-        user.cellphone = "55".concat(user.cellphone); // Add the Brazilian phone code as the default one
         user.customerId = environment.customerId;
         return this.http.post(`${environment.apiUrl}`, JSON.stringify(user), {
           headers: {
